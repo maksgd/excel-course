a3cfafa41fad023c5c135b40eb76fb5370c31ea4
maksgd <maksbossgd@inbox.ru>
2021-10-27 01:57:05 +0300

    Finish base table logic

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -1,21 +1,26 @@
-import {$} from '@core/dom'
+import { $ } from '@core/dom'
+import { Emitter } from '@core/Emitter'
 
 /* eslint-disable no-trailing-spaces */
 export class Excel {
     constructor(selector, options) {
         this.$el = $(selector)
         this.components = options.components || []
+        this.emitter = new Emitter()
     }
 
+    // Формирует блоки на странице
     getRoot() {
         const $root = $.create('div', 'excel')
 
+        const componentOptions = {
+            emitter: this.emitter
+        }
+
         this.components = this.components.map(Component => {
             const $el = $.create('div', Component.className)
-            const component = new Component($el)
-            // if (component.name) {
-            //     window['c' + component.name] = component
-            // }
+            const component = new Component($el, componentOptions)
+
             $el.html(component.toHTML())
             $root.append($el)
             return component
@@ -26,8 +31,11 @@ export class Excel {
 
     render() {
         this.$el.append(this.getRoot())
-        
+
         this.components.forEach(component => component.init())
     }
-}
 
+    destroy() {
+        this.components.forEach(component => component.destroy())
+    }
+}
